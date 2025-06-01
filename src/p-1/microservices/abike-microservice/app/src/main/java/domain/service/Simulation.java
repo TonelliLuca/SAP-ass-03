9d39aed0487d2b07f45b4fb1e4d65e4327089d76
Luca Tonelli <luca.tonelli11@studio.unibo.it>
2025-06-01 23:53:18 +0200

    :sparkles: feat: implement Simulation service for bike movement and event publishing

---
+++ src/p-1/microservices/abike-microservice/app/src/main/java/domain/service/Simulation.java
@@ -0,0 +1,81 @@
+package domain.service;
+
+import application.port.EventPublisher;
+import ddd.Service;
+import domain.events.ABikeArrivedToStation;
+import domain.events.ABikeArrivedToUser;
+import domain.events.ABikeUpdate;
+import domain.model.*;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+public class Simulation implements Service {
+    private ABike abike;
+    private final Destination destination;
+    public final String id;
+    private volatile boolean stopped  = false;
+    private static final double SPEED = 1.0;
+    private final Purpose purpose;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final EventPublisher publisher;
+    private final Vertx vertx;
+
+    public Simulation(ABike abike, Destination destination, Purpose purpose, EventPublisher eventPublisher, Vertx vertx) {
+        this.abike = abike;
+        this.destination = destination;
+        this.publisher = eventPublisher;
+        this.vertx = vertx;
+        this.purpose = purpose;
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public CompletableFuture<Void> start() {
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        logger.info("Starting Simulation");
+        vertx.setPeriodic(100, l -> {
+            synchronized (this) {
+                if (stopped) {
+                    vertx.cancelTimer(l);
+                    future.complete(null);
+                    return;
+                }
+                this.tick();
+            }
+        });
+        return future;
+    }
+
+    private void tick() {
+        P2d current = abike.position();
+        double dx = destination.position().x() - current.x();
+        double dy = destination.position().y() - current.y();
+        double distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < SPEED) {
+            abike = new ABike(abike.id(), destination.position(), abike.batteryLevel(), ABikeState.AVAILABLE);
+            if(purpose == Purpose.TO_USER)
+                publisher.publish(new ABikeArrivedToUser(abike.getId(), destination.getId()));
+            if(purpose == Purpose.TO_STATION)
+                publisher.publish(new ABikeArrivedToStation(abike.getId(), destination.getId()));
+            stop();
+            return;
+        }
+
+        double stepX = (dx / distance) * SPEED;
+        double stepY = (dy / distance) * SPEED;
+        P2d newPosition = new P2d(current.x() + stepX, current.y() + stepY);
+        abike = new ABike(abike.id(), newPosition, abike.batteryLevel(), abike.state());
+
+        publisher.publish(new ABikeUpdate(abike));
+    }
+
+    public void stop() {
+        stopped = true;
+    }
+
+}
